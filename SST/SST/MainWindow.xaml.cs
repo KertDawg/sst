bc96baf8017c31388a26cdd760746e725b0ea695
Kertis Henderson <kertishenderson@gmail.com>
2019-04-26 13:16:48 -0400

    Got basic functionality to work.

--- SST/SST/MainWindow.xaml.cs
+++ SST/SST/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,25 +14,74 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Renci.SshNet;
+
 
 namespace SST
 {
     public partial class MainWindow : Window
     {
+        protected SerialPort m_Serial;
+        protected SshClient m_SSHClient;
+        protected ShellStream m_SSHStream;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            m_Serial = new SerialPort();
+            LoadSerialPortBox();
         }
 
         private void ConnectButton_Click(object sender, RoutedEventArgs e)
         {
+            SshClient c;
+
+
             if (!AreParametersCorrect())
             {
                 return;
             }
+
+            if (!m_Serial.IsOpen)
+            {
+                m_Serial.PortName = SerialPortBox.SelectedValue.ToString();
+                m_Serial.BaudRate = int.Parse(BaudRateBox.SelectedValue.ToString());
+                m_Serial.Parity = (ParityBox.SelectedValue.ToString() == "None") ? Parity.None : Parity.None;
+                m_Serial.DataBits = int.Parse(BaudRateBox.SelectedValue.ToString());
+                m_Serial.StopBits = (StopBitsBox.SelectedValue.ToString() == "1")? StopBits.One : StopBits.None;
+                m_Serial.Handshake = (HandshakeBox.SelectedValue.ToString() == "None") ? Handshake.None : Handshake.XOnXOff;
+                m_Serial.Open();
+            }
+
+            WriteStatusLine("Connected to " + HostBox.Text);
+
+            m_SSHClient = new SshClient(HostBox.Text, int.Parse(PortBox.Text), UserNameBox.Text, PasswordInputBox.Password);
+            m_SSHClient.Connect();
+            m_SSHStream = m_SSHClient.CreateShellStream(TerminalTypeBox.SelectedValue.ToString(), uint.Parse(TerminalWidthBox.SelectedValue.ToString()), uint.Parse(TerminalHeightBox.SelectedValue.ToString()), 100, 100, 1024);
+            m_SSHStream.DataReceived += SSHDataReceived;
+            m_Serial.DataReceived += SerialDataReceived;
         }
 
-        protected Boolean AreParametersCorrect()
+        private void SSHDataReceived(object sender, Renci.SshNet.Common.ShellDataEventArgs e)
+        {
+            string Buffer;
+
+
+            Buffer = m_SSHStream.Read();
+            m_Serial.Write(Buffer);
+        }
+
+        private void SerialDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string Buffer;
+
+
+            Buffer = m_Serial.ReadExisting();
+            m_SSHStream.Write(Buffer);
+        }
+
+        protected bool AreParametersCorrect()
         {
             bool Response;
             StringBuilder Error;
@@ -82,6 +132,44 @@ namespace SST
             {
                 MessageBox.Show(Error.ToString(), "Can't Connect", MessageBoxButton.OK, MessageBoxImage.Error);
                 return false;
+            }
+        }
+
+        protected void WriteStatusLine(string MessageToWrite)
+        {
+            StatusArea.Text = StatusArea.Text + MessageToWrite + Environment.NewLine;
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            m_Serial.Close();
+            m_SSHStream.Close();
+            WriteStatusLine("Disconnected.");
+        }
+
+        protected void LoadSerialPortBox()
+        {
+            string[] Ports;
+            ComboBoxItem cbi;
+            bool First;
+
+
+            Ports = SerialPort.GetPortNames();
+            Array.Sort(Ports);
+            First = true;
+
+            foreach (string Port in Ports)
+            {
+                cbi = new ComboBoxItem();
+                cbi.Content = Port;
+
+                if (First)
+                {
+                    First = false;
+                    cbi.IsSelected = true;
+                }
+
+                SerialPortBox.Items.Add(cbi);
             }
         }
     }
